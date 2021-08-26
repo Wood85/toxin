89998443d4e8f6d386a315893de53ed61040b97c
Alexandr <megalistnn@gmail.com>
2021-08-26 22:04:20 +0300

    laid out some of the dropdowns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import * as $ from 'jquery'
 import './styles/style.scss'
+
+let isDrop = false
+$('.dropdown-select__trigger').click(function () {
+  if(!isDrop) {
+    $('.dropdown-select__dropdown').slideDown()
+  }else{
+    $('.dropdown-select__dropdown').slideUp()
+  }
+    isDrop = !isDrop
+})
