89b18751c7fab20b980b82e2e80ef93eb42da625
Alexandr <megalistnn@gmail.com>
2021-12-18 12:49:54 +0300

    pagination is ready

--- src/components/form-elements/pagination/pagination.js
+++ src/components/form-elements/pagination/pagination.js
@@ -1,77 +1,99 @@
-let totalElements = 170; // elements to show on the page
-let perPage = 12; // how many elements need to show per page
-let visibleLinks = 4; // how many links need to be visible;
+import * as $ from 'jquery';
 
-const linksCount = Math.ceil(totalElements / perPage); // pagination links count;
-const paginationContainer = document.querySelector('.pagination__panel');
-const informationLine = document.querySelector('.pagination__information-line');
+const rooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,
+  51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,
+  101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,
+  139,140,141,142,143,144,145,146,147,148,149,150, 151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176];
 
-// create the first array of links
-const paginationArray = [...Array(linksCount > visibleLinks ? visibleLinks + 1 : linksCount).keys()].slice(1);
-paginationArray.splice(visibleLinks - 1, 1, linksCount);
-
-// Pagination init function
-(function paginationInit(currentPage = 1) {
-  if (linksCount > visibleLinks) {
-    if ( linksCount - currentPage < visibleLinks - 1 && currentPage !== paginationArray[1] ) {
-      if (linksCount - currentPage >= visibleLinks - 2) {
-        currentPage = currentPage - 1;
-      } else if(currentPage !== paginationArray[paginationArray.length - 2] &&
-        currentPage !== paginationArray[paginationArray.length - 1]){
-        return;
-      }else {
-        currentPage = currentPage - 1 - (visibleLinks - 2 - (linksCount - currentPage));
-      }
-    } else if (currentPage !== 1) {
-      if (
-        currentPage > paginationArray[1] &&
-        currentPage < paginationArray[paginationArray.length - 2]
-      ) {
-        return;
-      } else if ( currentPage === paginationArray[1] ){
-        if( currentPage - (visibleLinks - 2) >= -1 ) {
-          currentPage = currentPage - (visibleLinks - 2) >= 2 ? currentPage - (visibleLinks - 2) : 1;
-        }else{
-          currentPage = 1;
-        }
-      } else {
-        currentPage -= 1;
-      }
-    } else {
-      currentPage = 1;
-    }
-    paginationArray.splice(1, paginationArray.length - 2,...Array(visibleLinks - 2)
-      .fill(currentPage)
-      .map((e, i) => (e += i + 1))
-    );
+function getPageList(totalPages, page, maxLength){
+  function range(start, end){
+    return Array.from(Array(end - start + 1), (_, i) => i + start);
   }
 
-  paginationContainer.innerHTML = '';
+  const sideWidth = maxLength < 9 ? 1 : 2,
+    leftWidth = (maxLength - sideWidth * 2 - 3) >> 1,
+    rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+  if(totalPages <= maxLength){
+    return range(1, totalPages);
+  }
 
-  paginationArray.map(e => {
-    let link = document.createElement('a');
-    link.classList.add('pagination__link');
-    link.textContent = e;
-    link.setAttribute('href', e);
-    paginationContainer.append(link);
+  if(page <= maxLength - sideWidth - 1 - rightWidth){
+    return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
+  }
+
+  if(page >= totalPages - sideWidth - 1 - rightWidth){
+    return range(1, sideWidth).concat(0, range(totalPages- sideWidth - 1 - rightWidth - leftWidth, totalPages));
+  }
+
+  return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
+}
+
+$(function(){
+  const numberOfItems = rooms.length,
+    limitPerPage = 12, //How many card items visible per a page
+    totalPages = Math.ceil(numberOfItems / limitPerPage),
+    paginationSize = 5; //How many page elements visible in the pagination
+  let currentPage;
+
+  function showPage(whichPage){
+    if(whichPage < 1 || whichPage > totalPages) return false;
+
+    currentPage = whichPage;
+
+    // $('.card-content .card').hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+    $('.pagination__item').slice(1, -1).remove();
+
+    getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+      $('<li>').addClass('pagination__item').addClass(item ? 'pagination__current-page' : 'pagination__dots')
+        .toggleClass('pagination__item_active', item === currentPage).append($('<a>').addClass('pagination__page-link')
+        .attr({href: 'javascript:void(0)'}).text(item || '...')).insertBefore('.pagination__button_next');
+    });
+    const startTrim = currentPage * limitPerPage - (limitPerPage - 1);
+    let endTrim = currentPage * limitPerPage,
+      amountOfItems = numberOfItems;
+
+    if(endTrim > amountOfItems) {
+      endTrim = amountOfItems;
+    }
+
+    if(+amountOfItems > 100) {
+      amountOfItems = '100+';
+    }else{
+      amountOfItems = numberOfItems;
+    }
+
+    $('.pagination__information-line').remove();
+
+    $('<div>').addClass('pagination__information-line').insertAfter('.pagination__panel');
+
+    $('.pagination__information-line')
+      .text(`${startTrim} – ${endTrim} из ${amountOfItems} вариантов аренды`);
+
+    $('.pagination__button_prev').toggleClass('pagination__item_disable', currentPage === 1);
+    $('.pagination__button_next').toggleClass('pagination__item_disable', currentPage === totalPages);
+    return true;
+  }
+
+  $('.pagination__panel').append(
+    $('<li>').addClass('pagination__item').addClass('pagination__button_prev').append($('<a>').addClass('pagination__page-link')
+      .attr({href: 'javascript:void(0)'})),
+    $('<li>').addClass('pagination__item').addClass('pagination__button_next').append($('<a>').addClass('pagination__page-link')
+      .attr({href: 'javascript:void(0)'}))
+  );
+  // $('.card-content').show();
+  showPage(1);
+
+  $(document).on('click', '.pagination__current-page:not(.pagination__item_active)', function(){
+    return showPage(+$(this).text());
   });
 
-  if(paginationArray[1] > 2) {
-    paginationContainer.children[1].before(' ... ');
-  }
-  if(paginationArray[paginationArray.length - 2] < linksCount - 1) {
-    paginationContainer.children[paginationArray.length - 1].before(' ... ');
-  }
+  $('.pagination__button_next').on('click', function(){
+    return showPage(currentPage + 1);
+  });
 
-  for(let i=0; i<paginationContainer.children.length; i++) {
-    paginationContainer.children[i].addEventListener('click', function (e) {
-      e.preventDefault();
-      paginationInit(+(e.target.getAttribute('href')));
-      console.log(+(e.target.getAttribute('href')));
-      paginationContainer.children[i].classList.add('pagination__link_active');
-    })
-  }
-})();
+  $('.pagination__button_prev').on('click', function(){
+    return showPage(currentPage - 1);
+  });
 
-
-
+});
