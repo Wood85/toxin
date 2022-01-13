8319a8b7cf656e81adb0ad88e6690a0ff113b402
Alexandr <megalistnn@gmail.com>
2022-01-13 21:19:02 +0300

    installed item-quantity-dropdown jQuery plugin

--- src/components/form-elements/dropdown-quantity/dropdown-quantity.js
+++ src/components/form-elements/dropdown-quantity/dropdown-quantity.js
@@ -1,14 +1,16 @@
-const getTemplate = (data=[], placeholder = 'Напиши свой placeholder') => {
-
-
+const getTemplate = (data=[], placeholder) => {
+  let text = placeholder;
+  if(!placeholder) {
+    text = 'Напиши свой placeholder';
+  }
   const items = data.map(item => {
     return `
       <li class="dropdown-quantity__item" data-type="item" data-id="${item.id}">
         <div class="dropdown-quantity__item-title">${item.title}</div>
         <div class="dropdown-quantity__block-quantity">
-          <div class="dropdown-quantity__button-minus" data-type="minus">-</div>       
-          <div class="dropdown-quantity__value" data-type="value">${item.value}</div>
-          <div class="dropdown-quantity__button-plus" data-type="plus">+</div>
+          <div class="dropdown-quantity__button-minus" data-id="${item.id}" data-type="minus">-</div>       
+          <div class="dropdown-quantity__value" data-id="${item.id}" data-type="value">${item.value}</div>
+          <div class="dropdown-quantity__button-plus" data-id="${item.id}" data-type="plus">+</div>
         </div>  
       </li>
     `
@@ -16,7 +18,7 @@ const getTemplate = (data=[], placeholder = 'Напиши свой placeholder')
 
   return `
     <div class=dropdown-quantity__input data-type="input">
-      ${placeholder}
+      <span class=dropdown-quantity__text data-type="text">${text}</span>
     </div>  
     <div class=dropdown-quantity__dropdown>
       <ul class="dropdown-quantity__list">
@@ -30,37 +32,75 @@ export class DropdownQuantity {
   constructor(selector, options) {
     this.el  = document.querySelector(selector);
     this.options = options;
+    // this.selectedId = null;
 
-    this.render();
-    this.setup();
+    this._render();
+    this._setup();
   };
 
-  render() {
+  _render() {
     const{placeholder, data} = this.options
     this.el.classList.add('dropdown-quantity');
     this.el.innerHTML = getTemplate(data, placeholder);
   };
 
-  setup() {
+  _setup() {
     this.clickHandler = this.clickHandler.bind(this);
     this.el.addEventListener('click', this.clickHandler);
+    this.text = this.el.querySelector('[data-type="text"]');
+    this.values = this.el.querySelectorAll('[data-type="value"]');
   };
 
   clickHandler(event) {
-    const {type} = event.target.dataset
-    if(type === 'input') {
+    const {type} = event.target.dataset;
+    const arrayValues = [this.options.data[0].value, this.options.data[1].value, this.options.data[2].value];
+    const arrayValuesCopy = arrayValues.slice();
+    if(type === 'input' || type === 'text') {
       this.toggle();
     } else if (type ==='plus') {
-      event.target.previousElementSibling.textContent = parseInt(event.target.previousElementSibling.textContent,10)+1
-    } else if (type ==='minus') {
-      event.target.nextElementSibling.textContent = parseInt(event.target.nextElementSibling.textContent,10)-1
+      const id = event.target.dataset.id;
+      const value = event.target.previousElementSibling;
+      value.innerHTML = parseInt(value.textContent, 10)+1;
+      console.log(`value : ${value.textContent}, id : ${id}`);
+      if(id === '1') {
+        arrayValuesCopy[0] = value.textContent;
+        this.text.textContent = `${this.options.data[0].title} ${arrayValuesCopy[0]} ${this.options.data[1].title} ${arrayValuesCopy[1]} ${this.options.data[2].title} ${arrayValuesCopy[2]}`;
+      }
+      if(id === '2') {
+        arrayValuesCopy[1] = value.textContent;
+        this.text.textContent = `${this.options.data[0].title} ${arrayValuesCopy[0]} ${this.options.data[1].title} ${arrayValuesCopy[1]} ${this.options.data[2].title} ${arrayValuesCopy[2]}`;
+      if(id === '3') {
+        arrayValuesCopy[2] = value.textContent;
+        this.text.textContent = `${this.options.data[0].title} ${arrayValuesCopy[0]} ${this.options.data[1].title} ${arrayValuesCopy[1]} ${this.options.data[2].title} ${arrayValuesCopy[2]}`;
     }
 
+    } else if (type ==='minus') {
+        const id = event.target.dataset.id;
+        const value = event.target.nextElementSibling;
+        value.innerHTML = parseInt(value.textContent, 10) - 1;
+        console.log(`value : ${value.textContent}, id : ${id}`);
+        // if (id === '1') {
+        //   this.text.textContent = `${this.options.data[0].title} ${value.textContent} ${this.options.data[1].title} ${this.options.data[2].title}`;
+        // } else if (id === '2') {
+        //   this.text.textContent = `${this.options.data[0].title} ${this.options.data[1].title} ${value.textContent} ${this.options.data[2].title}`;
+        // } else if (id === '3') {
+        //   this.text.textContent = `${this.options.data[0].title} ${this.options.data[1].title} ${this.options.data[2].title} ${value.textContent}`;
+        // }
+      }
+    }
   };
 
   get isOpen() {
     return this.el.classList.contains('open');
-  }
+  };
+
+  // get current() {
+  //   return this.options.data.find(item => item.id === this.selectedId)
+  // }
+  //
+  // count(id) {
+  //   this.selectedId = id;
+  // };
 
   toggle() {
     this.isOpen ? this.close() : this.open();
@@ -72,13 +112,6 @@ export class DropdownQuantity {
 
   close() {
     this.el.classList.remove('open');
-  };
-
-
-
-  quantity() {
-    const values = this.el.querySelectorAll('dropdown-quantity__value');
-
   };
 
   destroy() {
--- src/components/form-elements/dropdown-selection/dropdown-selection.js
+++ src/components/form-elements/dropdown-selection/dropdown-selection.js
@@ -1,14 +1,17 @@
-import '../quantity-change-block/quantity-change-block'
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min';
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
-//Дропдаун
-let isDrop = false;
-const $trigger = $('.js-dropdown-selection__trigger');
-$trigger.click(function () {
-  const $dropdown = $('.js-dropdown-selection__dropdown');
-  if(!isDrop) {
-    $dropdown.slideDown();
-  }else{
-    $dropdown.slideUp();
-  }
-  isDrop = !isDrop;
-})
+$(document).ready(() => {
+  $('.iqdropdown').iqDropdown({
+    maxItems: Infinity,
+    minItems: 0,
+    selectionText: 'item',
+    textPlural: 'items',
+    controls: {
+      position: 'right',
+      displayCls: 'iqdropdown-item-display',
+      controlsCls: 'iqdropdown-item-controls',
+      counterCls: 'counter'
+    }
+  })
+});
