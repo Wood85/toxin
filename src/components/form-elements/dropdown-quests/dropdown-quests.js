50fcf941105a5aed317ffd1e422f4d41da021bff
Alexandr <megalistnn@gmail.com>
2022-01-15 21:56:33 +0300

    finished work with dropdown of choosing the number of bedrooms, began to redo the dropdown of the number of guests

--- src/components/form-elements/dropdown-quests/dropdown-quests.js
+++ src/components/form-elements/dropdown-quests/dropdown-quests.js
@@ -1,29 +1,69 @@
-function dropdownQuests() {
-  //Дропдаун
-  let isDrop = false;
-  const $trigger = $('.js-dropdown-quests__trigger'),
-    $dropdown = $('.dropdown-quests__dropdown'),
-    $backdrop = $('.dropdown-quests__backdrop'),
-    $adults = $(".button-minus[data-title='adults']"),
-    $children = $(".button-minus[data-title='children']"),
-    $babies = $(".button-minus[data-title='babies']");
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min';
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
-  $trigger.click(function () {
-    if(!isDrop) {
-      $dropdown.slideDown();
-      $backdrop.css('border-radius', '4px 4px 0 0 ');
-      $adults.addClass('limit');
-      $children.addClass('limit');
-      $babies.addClass('limit');
-    }else{
-      $dropdown.slideUp();
-      setTimeout(resetCSS, 400);
-      resetCSS();
+$(document).ready(() => {
+  const $iqdropdown = $('.iqdropdown'),
+    $input = $('.iqdropdown-selection');
+  $iqdropdown.iqDropdown({
+    maxItems: Infinity,
+    minItems: 0,
+    controls: {
+      position: 'right',
+      displayCls: 'iqdropdown-item-display',
+      controlsCls: 'iqdropdown-item-controls',
+      counterCls: 'counter',
+    },
+    setSelectionText: (itemCount, totalItems) => {
+
+      let quests = 'гость',
+        babies = 'младенец';
+
+      let inputText = `${itemCount.adults + itemCount.kids} ${quests}, ${itemCount.babies} ${babies}`;
+
+      if(totalItems === 0) {
+        inputText = `Сколько гостей`;
+      }
+
+      $input.text(inputText);
+
+    },
+    onChange: (id, count, totalItems) => {
+      console.log(id, count, totalItems)
+      if(id === 'adults' && count === 0) {
+        $('[data-id="adults"] .button-decrement').addClass('iqdropdown-limit');
+      }else if(id === 'adults' && count !== 0) {
+        $('[data-id="adults"] .button-decrement').removeClass('iqdropdown-limit');
+      }
+      if(id === 'adults' && count === 10) {
+        $('[data-id="adults"] .button-increment').addClass('iqdropdown-limit');
+      }else if(id === 'adults' && count !== 10) {
+        $('[data-id="adults"] .button-increment').removeClass('iqdropdown-limit');
+      }
+
+      if(id === 'kids' && count === 0) {
+        $('[data-id="kids"] .button-decrement').addClass('iqdropdown-limit');
+      }else if(id === 'kids' && count !== 0) {
+        $('[data-id="kids"] .button-decrement').removeClass('iqdropdown-limit');
+      }
+      if(id === 'kids' && count === 10) {
+        $('[data-id="kids"] .button-increment').addClass('iqdropdown-limit');
+      }else if(id === 'kids' && count !== 10) {
+        $('[data-id="kids"] .button-increment').removeClass('iqdropdown-limit');
+      }
+
+      if(id === 'babies' && count === 0) {
+        $('[data-id="babies"] .button-decrement').addClass('iqdropdown-limit');
+      }else if(id === 'babies' && count !== 0) {
+        $('[data-id="babies"] .button-decrement').removeClass('iqdropdown-limit');
+      }
+      if(id === 'babies' && count === 10) {
+        $('[data-id="babies"] .button-increment').addClass('iqdropdown-limit');
+      }else if(id === 'babies' && count !== 10) {
+        $('[data-id="babies"] .button-increment').removeClass('iqdropdown-limit');
+      }
     }
-    isDrop = !isDrop;
-    function resetCSS() {
-      $backdrop.css('border-radius', '4px')
-    }
-  })
-}
-dropdownQuests();
+  });
+});
+
+
+
--- src/components/form-elements/dropdown-selection/dropdown-selection.js
+++ src/components/form-elements/dropdown-selection/dropdown-selection.js
@@ -1,6 +1,7 @@
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min';
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
+
 $(document).ready(() => {
   const $iqdropdown = $('.iqdropdown'),
     $input = $('.iqdropdown-selection');
@@ -13,9 +14,66 @@ $(document).ready(() => {
       controlsCls: 'iqdropdown-item-controls',
       counterCls: 'counter',
     },
-    setSelectionText: (itemCount, totalItems) => {
-      $input.text(`${itemCount.bedrooms} ${itemCount.beds} ${itemCount.bathrooms}`)
-      console.log(itemCount, totalItems)
+    setSelectionText: (itemCount) => {
+      let bedrooms = 'спальня',
+        beds = 'кровать';
+
+      if(itemCount.bedrooms > 1 && itemCount.bedrooms < 5) {
+        bedrooms = 'спальни';
+      }
+
+      if(itemCount.bedrooms > 4 && itemCount.bedrooms < 11) {
+        bedrooms = 'спален';
+      }
+
+      if(itemCount.beds > 1 && itemCount.beds < 5) {
+        beds = 'кровати';
+      }
+
+      if(itemCount.beds > 4 && itemCount.beds < 11) {
+        beds = 'кроватей';
+      }
+
+      let inputText = `${itemCount.bedrooms} ${bedrooms}, ${itemCount.beds} ${beds}...`;
+      $input.text(inputText);
+
+      $('[data-id="bathrooms"] .button-decrement').addClass('iqdropdown-limit');
+
+    },
+    onChange: (id, count, totalItems) => {
+      console.log(id, count, totalItems)
+      if(id === 'bedrooms' && count === 1) {
+        $('[data-id="bedrooms"] .button-decrement').addClass('iqdropdown-limit');
+      }else if(id === 'bedrooms' && count !== 1) {
+        $('[data-id="bedrooms"] .button-decrement').removeClass('iqdropdown-limit');
+      }
+      if(id === 'bedrooms' && count === 10) {
+        $('[data-id="bedrooms"] .button-increment').addClass('iqdropdown-limit');
+      }else if(id === 'bedrooms' && count !== 10) {
+        $('[data-id="bedrooms"] .button-increment').removeClass('iqdropdown-limit');
+      }
+
+      if(id === 'beds' && count === 1) {
+        $('[data-id="beds"] .button-decrement').addClass('iqdropdown-limit');
+      }else if(id === 'beds' && count !== 1) {
+        $('[data-id="beds"] .button-decrement').removeClass('iqdropdown-limit');
+      }
+      if(id === 'beds' && count === 10) {
+        $('[data-id="beds"] .button-increment').addClass('iqdropdown-limit');
+      }else if(id === 'beds' && count !== 10) {
+        $('[data-id="beds"] .button-increment').removeClass('iqdropdown-limit');
+      }
+
+      if(id === 'bathrooms' && count === 0) {
+        $('[data-id="bathrooms"] .button-decrement').addClass('iqdropdown-limit');
+      }else if(id === 'bathrooms' && count !== 0) {
+        $('[data-id="bathrooms"] .button-decrement').removeClass('iqdropdown-limit');
+      }
+      if(id === 'bathrooms' && count === 10) {
+        $('[data-id="bathrooms"] .button-increment').addClass('iqdropdown-limit');
+      }else if(id === 'bathrooms' && count !== 10) {
+        $('[data-id="bathrooms"] .button-increment').removeClass('iqdropdown-limit');
+      }
     }
   });
 });
